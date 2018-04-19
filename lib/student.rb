d267f36db825e4a227ba64072f8a27f394d7ab05
Trevor Seitz <trevinla@gmail.com>
2018-04-19 17:03:07 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -89,7 +89,7 @@ class Student
     end
   end
 
-  def self.first_X_students_in_grade_10(num)
+  def self.first_X_students_in_grade_10
     sql = <<-SQL
     SELECT *
     FROM students
@@ -98,7 +98,7 @@ class Student
     SQL
 
     DB[:conn].execute(sql, num).map do |row|
-      self.new_from_db(row, num)
+      self.new_from_db(row)
     end
   end
 
