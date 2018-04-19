023bedfba1bcac42f1426c74dbc65dbb3917103c
Trevor Seitz <trevinla@gmail.com>
2018-04-19 16:58:30 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -90,13 +90,13 @@ class Student
   end
 
   def self.first_X_students_in_grade_10(num)
-    binding.pry
     sql = <<-SQL
     SELECT *
     FROM students
     WHERE grade = 10
     LIMIT num
     SQL
+      binding.pry
 
     DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
